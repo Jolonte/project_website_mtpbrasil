8dfcee65f8d6b23636c0364f4e30516af4bab8e4
Jolonte <vinicius.nevesco@gmail.com>
2023-04-05 11:40:18 -0300

    :sparkles: feat: Header & Hero

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,42 @@
 
 </script>
 
-<main>
-
-</main>
+<div class="flex flex-col px-[12.25rem]">
+    <header class="flex flex-col items-center pt-[3.438rem]">
+        <nav class="flex w-[57.313rem]">
+            <img src="src/assets/mtp_logo.png" alt="Logo MTP">
+            <ul class="flex w-full justify-center gap-[2.313rem] uppercase">
+                <li>Sobre a MTP</li>
+                <li>Serviços</li>
+                <li>Setores</li>
+                <li>Cases</li>
+                <li>Carreira</li>
+                <li>Blog</li>
+                <li>Contato</li>
+            </ul>
+            <!-- imagem responsável por manter padding equilibrado -->
+            <img class="opacity-0" src="src/assets/mtp_logo.png" alt="Logo MTP">
+        </nav>
+        <!-- Hero Section -->
+        <div class="flex flex-col items-center gap-[1.063rem] pt-[2.688rem]">
+            <h1 class="font-bold text-5xl text-center">Uma empresa com 25 anos de história, em constante <span class="text-blue-600">crescimento</span></h1>
+            <div class="pt-[1.5rem]">
+                <ul class="flex gap-[2.5rem] w-full justify-center">
+                    <li class="flex items-center gap-[0.5rem]">
+                        <i class="material-symbols-outlined text-blue-600">done</i>
+                        Satisfação do cliente
+                    </li>
+                    <li class="flex items-center gap-[0.5rem]">
+                        <i class="material-symbols-outlined text-blue-600">done</i>
+                        Soluções personalizadas
+                    </li>
+                    <li class="flex items-center gap-[0.5rem]">
+                        <i class="material-symbols-outlined text-blue-600">done</i>
+                        Expertise comprovada
+                    </li>
+                </ul>
+            </div>
+            <img class="mt-[3rem] w-[40rem]" src="src/assets/hero_image.svg" alt="Logo MTP">
+        </div>
+    </header>
+</div>
