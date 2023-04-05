fa2ccfd18bab69cd04b07e1103bd6e230e8128d5
Jolonte <vinicius.nevesco@gmail.com>
2023-04-05 13:13:47 -0300

    :recycle: Refactor

--- src/App.svelte
+++ src/App.svelte
@@ -5,7 +5,7 @@
 <div class="flex flex-col">
     <header class="flex flex-col items-center pt-[3.438rem] px-[12.25rem] h-screen">
         <nav class="flex w-[57.313rem]">
-            <img src="src/assets/mtp_logo.png" alt="Logo MTP">
+            <img src="/mtp_logo.png" alt="Logo MTP">
             <ul class="flex w-full justify-center gap-[2.313rem] uppercase">
                 <li>Sobre a MTP</li>
                 <li>Serviços</li>
@@ -16,7 +16,7 @@
                 <li>Contato</li>
             </ul>
             <!-- imagem responsável por manter padding equilibrado -->
-            <img class="opacity-0" src="src/assets/mtp_logo.png" alt="Logo MTP">
+            <img class="opacity-0" src="/mtp_logo.png" alt="Logo MTP">
         </nav>
         <!-- Hero Section -->
         <div class="flex flex-col items-center gap-[1.063rem] pt-[2.688rem]">
@@ -37,17 +37,17 @@
                     </li>
                 </ul>
             </div>
-            <img class="mt-[3rem] w-[40rem]" src="src/assets/hero_image.svg" alt="Logo MTP">
+            <img class="mt-[3rem] w-[40rem]" src="/hero_image.svg" alt="Logo MTP">
         </div>
     </header>
     <main>
         <div class="flex flex-col items-center py-[3.5rem] gap-[2.5rem] bg-black">
             <h2 class="text-3xl font-semibold text-white">Trabalhamos com as melhores empresas</h2>
             <div class="flex justify-center gap-[3.25rem]">
-                <img class="h-20" src="src/assets/coca_cola.png" alt="Coca-Cola">
-                <img class="h-20" src="src/assets/avon.png" alt="Avon">
-                <img class="h-20" src="src/assets/enel.png" alt="Enel">
-                <img class="h-20" src="src/assets/bayern.png" alt="Bayern">
+                <img class="h-20" src="/coca_cola.png" alt="Coca-Cola">
+                <img class="h-20" src="/avon.png" alt="Avon">
+                <img class="h-20" src="/enel.png" alt="Enel">
+                <img class="h-20" src="/bayern.png" alt="Bayern">
             </div>
             <p class="text-blue-600">Conheça todos os nossos clientes ></p>
         </div>
