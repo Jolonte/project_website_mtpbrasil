1a79ad42450f09e6a78cb58056a0769cb39f1a99
Jolonte <vinicius.nevesco@gmail.com>
2023-04-05 22:11:01 -0300

    :sparkles: feat: Main

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="flex flex-col">
-    <header class="flex flex-col items-center pt-[3.438rem] px-[12.25rem] h-screen">
+    <header class="flex flex-col items-center pt-[3.438rem] px-[12.25rem] h-screen bg-[#dfe6e9]">
         <nav class="flex w-[57.313rem]">
             <img src="/mtp_logo.png" alt="Logo MTP">
             <ul class="flex w-full justify-center gap-[2.313rem] uppercase">
@@ -37,7 +37,8 @@
                     </li>
                 </ul>
             </div>
-            <img class="mt-[3rem] w-[40rem]" src="/hero_image.svg" alt="Logo MTP">
+            <button class="font-semibold text-white mt-[0.5rem] py-[0.625rem] px-[1.125rem] rounded-[0.938rem] bg-blue-600 ">Mais informações</button>
+            <img class="mt-[3rem] w-[35rem]" src="/hero_image.svg" alt="Imagem: Trabalho em equipe">
         </div>
     </header>
     <main>
@@ -49,7 +50,46 @@
                 <img class="h-20" src="/enel.png" alt="Enel">
                 <img class="h-20" src="/bayern.png" alt="Bayern">
             </div>
-            <p class="text-blue-600">Conheça todos os nossos clientes ></p>
+            <p class="font-bold text-blue-600">Conheça todos os nossos clientes ></p>
+        </div>
+        <div class="flex flex-col py-[9.5rem]">
+            <section class="flex flex-col items-center text-center gap-[4.5rem]">
+                <div class="flex flex-col items-center gap-[1.438rem]">
+                    <h2 class="text-4xl font-bold capitalize"><span class="text-blue-600">Potencializando</span> seu <span class="text-blue-600">sucesso</span> com nossos serviços</h2>
+                    <p class="w-2/4">A MTP é líder em Serviços de Garantia Digital, com 25 anos de experiência global. Seu compromisso com a qualidade, inovação e adaptação às mudanças do mercado é integrado ao ambiente de Digital Business Assurance.</p>
+                </div>
+                <div class="grid grid-cols-2 grid-rows-2 justify-center gap-[2.688rem]">
+                    <div class="flex flex-col gap-[1.438rem]">
+                        <i class="material-symbols-outlined text-blue-600 text-7xl">workspace_premium</i>
+                        <div class="flex flex-col gap-[0.813rem]">
+                            <h3 class="font-bold text-2xl">Garantia da qualidade</h3>
+                            <p class="w-[25rem]">Atuamos em todas as fases da atividade comercial de software, desde o desenvolvimento até a garantia de qualidade, visando uma melhoria contínua.</p>
+                        </div>
+                    </div>
+                    <div class="flex flex-col gap-[1.438rem]">
+                        <i class="material-symbols-outlined text-blue-600 text-7xl">touch_app</i>
+                        <div class="flex flex-col gap-[0.813rem]">
+                            <h3 class="font-bold text-2xl">Experiência do usuário</h3>
+                            <p class="w-[25rem]">Desenvolvimento de software completo, da criação à garantia de qualidade, para melhoria contínua em todas as etapas comerciais.</p>
+                        </div>
+                    </div>
+                    <div class="flex flex-col gap-[1.438rem]">
+                        <i class="material-symbols-outlined text-blue-600 text-7xl">shield</i>
+                        <div class="flex flex-col gap-[0.813rem]">
+                            <h3 class="font-bold text-2xl">Segurança</h3>
+                            <p class="w-[25rem]">A MTP oferece soluções e práticas em segurança cibernética, detectando e prevenindo vulnerabilidades em aplicativos de organizações.</p>
+                        </div>
+                    </div>
+                    <div class="flex flex-col gap-[1.438rem]">
+                        <i class="material-symbols-outlined text-blue-600 text-7xl">cycle</i>
+                        <div class="flex flex-col gap-[0.813rem]">
+                            <h3 class="font-bold text-2xl">DevOps</h3>
+                            <p class="w-[25rem]">Cobertura completa do ciclo de vida do software em iniciativas digitais, incluindo aplicativos móveis e de e-business para vários dispositivos.</p>
+                        </div>
+                    </div>
+                </div>
+                <button class="w-fit font-semibold text-white mt-[0.5rem] py-[0.625rem] px-[1.125rem] rounded-[0.938rem] bg-blue-600 ">Mais informações</button>
+            </section>
         </div>
     </main>
 </div>
