06b16de54f832cd39ef3b18c533f83b6740f4376
Jolonte <vinicius.nevesco@gmail.com>
2023-04-07 12:53:52 -0300

    :sparkles: Contact

--- src/App.svelte
+++ src/App.svelte
@@ -13,14 +13,13 @@
                 <li>Cases</li>
                 <li>Carreira</li>
                 <li>Blog</li>
-                <li>Contato</li>
             </ul>
             <!-- imagem responsável por manter padding equilibrado -->
             <img class="opacity-0" src="/mtp_logo.png" alt="Logo MTP">
         </nav>
         <!-- Hero Section -->
         <div class="flex flex-col items-center gap-[1.063rem] pt-[2.688rem]">
-            <h1 class="font-bold text-5xl text-center">Uma empresa com 25 anos de história, em constante <span class="text-blue-600">crescimento</span></h1>
+            <h1 class="font-bold text-5xl text-center text-[#2f2e41]">Uma empresa com 25 anos de história, em constante <span class="text-blue-600">crescimento</span></h1>
             <div class="pt-[1.5rem]">
                 <ul class="flex gap-[2.5rem] w-full justify-center">
                     <li class="flex items-center gap-[0.5rem]">
@@ -42,7 +41,7 @@
         </div>
     </header>
     <main>
-        <div class="flex flex-col items-center py-[3.5rem] gap-[2.5rem] bg-black">
+        <div class="flex flex-col items-center py-[3.5rem] gap-[2.5rem] bg-[#2f2e41]">
             <h2 class="text-3xl font-semibold text-white">Trabalhamos com as melhores empresas</h2>
             <div class="flex justify-center gap-[3.25rem]">
                 <img class="h-20" src="/coca_cola.png" alt="Coca-Cola">
@@ -55,7 +54,7 @@
         <div class="flex flex-col py-[9.5rem]">
             <section class="flex flex-col items-center text-center gap-[4.5rem] mb-[11.688rem]">
                 <div class="flex flex-col items-center gap-[1.438rem]">
-                    <h2 class="text-4xl font-bold capitalize"><span class="text-blue-600">Potencializando</span> seu <span class="text-blue-600">sucesso</span> com nossos serviços</h2>
+                    <h2 class="text-4xl font-bold capitalize text-[#2f2e41]"><span class="text-blue-600">Potencializando</span> seu <span class="text-blue-600">sucesso</span> com nossos serviços</h2>
                     <p class="w-2/4">A MTP é líder em Serviços de Garantia Digital, com 25 anos de experiência global. Seu compromisso com a qualidade, inovação e adaptação às mudanças do mercado é integrado ao ambiente de Digital Business Assurance.</p>
                 </div>
                 <div class="grid grid-cols-2 grid-rows-2 justify-center gap-[2.688rem]">
@@ -90,10 +89,45 @@
                 </div>
                 <button class="w-fit font-semibold text-white mt-[0.5rem] py-[0.625rem] px-[1.125rem] rounded-[0.938rem] bg-blue-600 ">Mais informações</button>
             </section>
+            <section class="flex flex-col items-center py-[7.5rem] px-[12.25rem] bg-[#2f2e41] mb-[7.5rem] gap-[2rem]">
+                <div class="flex flex-col items-center gap-[2rem]">
+                    <h2 class="text-4xl font-semibold text-white">Entre em contato</h2>
+                    <div class="flex justify-center gap-[1.5rem]">
+                        <button class="w-[8rem] font-semibold text-white py-[0.625rem] px-[1.125rem] rounded-[0.938rem] border-[0.1rem] border-blue-600">Brasil</button>
+                        <button class="w-[8rem] font-semibold text-white py-[0.625rem] px-[1.125rem] rounded-[0.938rem] border-[0.1rem] border-blue-600">México</button>
+                        <button class="w-[8rem] font-semibold text-white py-[0.625rem] px-[1.125rem] rounded-[0.938rem] bg-blue-600">Espanha</button>
+                    </div>
+                </div>
+                <div class="flex flex-col w-[55rem] rounded-[0.938rem] drop-shadow-xl">
+                    <div class="flex flex-col w-full p-[2rem] gap-[2rem] rounded-[0.938rem] bg-[#dfe6e9]">
+                        <div class="flex justify-center flex-wrap gap-[0.5rem]">
+                            <button class="w-[8rem] text-white py-[0.625rem] px-[1.125rem] rounded-[0.938rem] border-[0.1rem] bg-[#2f2e41]">Madrid</button>
+                            <button class="w-[8rem] text-[#2f2e41] py-[0.625rem] px-[1.125rem] rounded-[0.938rem] border-[0.1rem] border-[#2f2e41]">Alicante</button>
+                            <button class="w-[8rem] text-[#2f2e41] py-[0.625rem] px-[1.125rem] rounded-[0.938rem] border-[0.1rem] border-[#2f2e41]">Sevilla</button>
+                            <button class="w-[8rem] text-[#2f2e41] py-[0.625rem] px-[1.125rem] rounded-[0.938rem] border-[0.1rem] border-[#2f2e41]">Almería</button>
+                            <button class="w-[8rem] text-[#2f2e41] py-[0.625rem] px-[1.125rem] rounded-[0.938rem] border-[0.1rem] border-[#2f2e41]">Barcelona</button>
+                        </div>
+                        <div class="h-[20rem] w-full rounded-[0.938rem] bg-slate-400"></div>
+                        <div class="flex flex-col gap-[0.5rem]">
+                            <p>Endereço: Rua Correia Dias, 337 - 4º Andar Paraíso. CEP: 04104-901</p>
+                            <p>Telefone: +55 (11) 5525-0650</p>
+                            <p>E-mail: contato@mtp.com.br</p>
+                        </div>
+                        <h3 class="font-bold text-2xl">Fale conosco</h3>
+                        <form class="flex flex-col items-center gap-[2rem]" action="POST" >
+                            <input class="w-full py-[0.625rem] px-[1.125rem] rounded-[0.938rem]" placeholder="Nome e sobrenome*" type="text" name="Nome">
+                            <input class="w-full py-[0.625rem] px-[1.125rem] rounded-[0.938rem]" placeholder="E-mail*" type="text" name="Email">
+                            <input class="w-full py-[0.625rem] px-[1.125rem] rounded-[0.938rem]" placeholder="Número de celular" type="text" name="Telefone">
+                            <textarea class="w-full py-[0.625rem] px-[1.125rem] rounded-[0.938rem] h-40 resize-none" placeholder="Como podemos ajudá-lo?*" name="Consulta"></textarea>
+                            <button type="submit" class="w-[8rem] font-semibold text-white py-[0.625rem] px-[1.125rem] rounded-[0.938rem] border-[0.1rem] bg-blue-600">Enviar</button>
+                        </form>
+                    </div>
+                </div>
+            </section>
             <!-- objeto com "props" que da para por html dentro -->
             <section class="flex flex-col items-center text-center gap-[4.5rem]">
                 <div class="flex flex-col items-center gap-[1.438rem]">
-                    <h2 class="text-4xl font-bold capitalize">Nossas certificações</h2>
+                    <h2 class="text-4xl font-bold capitalize text-[#2f2e41]">Nossas certificações</h2>
                     <p class="w-2/4">Buscamos constantemente certificações e aprimoramento de qualidade atestando a conformidade do nosso sistema aos mais rigorosos padrões internacionais.</p>
                 </div>
                 <div class="flex gap-[4.5rem]">
